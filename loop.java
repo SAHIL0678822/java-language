b4d1483518e2e141bcdb22d763e904f3d44f8f56
Sahil Ansari <SAHIL0678822@GMAIL.COM>
2026-01-15 10:45:19 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,21 @@
+
+
+public class loop {
+    public static void main(String[] args) {
+        // for loop (intialization; condition; increment/decrement)
+        // 
+        
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i = i+1;  //i++;       }
+
+        int i = 0;
+        do {
+            System.out.println(i);
+            i = i + 1;
+        } while(i < 11);    
+        }
+    }
+    
+
