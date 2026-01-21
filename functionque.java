4932603893a9edf938cf8bf09443187aabd26759
Sahil Ansari <SAHIL0678822@GMAIL.COM>
2026-01-21 09:30:06 +0530

    Add files via upload

---
+++ functionque.java
@@ -0,0 +1,56 @@
+// import java.util.*;
+// public class functionque {
+//     public static int calculatesum(int a, int b){
+//         int sum = a + b;
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = calculatesum(a, b);
+//         System.out.println(sum);
+//         sc.close();
+//     }
+    
+// }
+
+
+
+// import java.util.*;
+// public class functionque {
+//     public static int calculateMultiple(int a, int b){
+//         int calculateMultiple = a * b;
+//         return calculateMultiple;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int Multiple = calculateMultiple(a, b);
+//         System.out.println(Multiple);
+//         sc.close();
+//     }
+    
+// }
+
+
+
+
+
+import java.util.*;
+public class functionque {
+    public static int calculateMultiple(int a, int b){
+        int calculateMultiple = a * b;
+        return calculateMultiple;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int Multiple = calculateMultiple(a, b);
+        System.out.println(Multiple);
+        sc.close();
+    }
+    
+}
