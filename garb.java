2c382660423580b4e7fc1bcd1bfc3c9175a5c92b
Sahil Ansari <SAHIL0678822@GMAIL.COM>
2026-02-16 12:03:36 +0530

    Add files via upload

---
+++ garb.java
@@ -0,0 +1,36 @@
+// public class garb {
+//     public static void main (String [] args){
+//         int[] arr  = {10, 20, 30, 40, 10};
+//         // to print desired element of array
+//         // System.out.println(arr[1]);
+
+//         // to last element of array
+//         // System.out.println(arr[arr.length-1]);
+     
+//         // to print sum of first and last element of array
+//         // int sum = arr[0] + arr[arr.length-1];
+//         // System.out.println(sum);
+
+
+// // to compare first and last element of array
+// //        if (arr[0] > arr[arr.length - 1]) {
+// //     System.out.println("first element is bigger");
+// // } 
+// // else if (arr[0] < arr[arr.length - 1]) {
+// //     System.out.println("last element is bigger");
+// // }
+// // else {
+// //     System.out.println("both are equal");
+// // }
+
+//         }
+//     }
+
+
+public class garb{
+    public static void main(String[] args){
+        for (int i = 1; i <=5; i++){
+            System.out.println(i);
+        }
+    }
+}
